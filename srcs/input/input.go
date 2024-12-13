9007afb1ab1cfe20fc33b35ee4388f9e6b2131e7
Samuel Cordero <sacorder@student.42madrid.com>
2024-12-13 14:29:15 +0100

    basic loop done, add fucntionality

--- srcs/input/input.go
+++ srcs/input/input.go
@@ -1,8 +1,86 @@
 package input
 
-// import (
-// 	"github.com/chzyer/readline"
-// )
+import (
+	"fmt"
+	"strings"
+
+	"github.com/chzyer/readline"
+)
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+func RunShell(commandChan chan Command, ackChan chan struct{}) {
+	rl, err := readline.New("taskmaster> ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil { // EOF
+			close(commandChan)
+			break
+		}
+
+		// Trim spaces
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		// Split command and arguments
+		parts := strings.Fields(line)
+		command := parts[0]
+		args := parts[1:]
+
+		// Send through the channel
+		commandChan <- Command{Name: command, Args: args}
+
+		// Wait for main thread to process
+		<-ackChan
+
+		// Exit if "exit"
+		if command == "exit" {
+			break
+		}
+	}
+}
+
+func CheckForCommands(commandChan chan Command, ackChan chan struct{}) {
+	for {
+		cmd, ok := <-commandChan
+		if !ok {
+			fmt.Println("Command channel closed. Exiting.")
+			close(ackChan)
+			return
+		}
+
+		switch cmd.Name {
+		case "help":
+			fmt.Println("Available commands: help, start, stop, restart, reload, exit")
+		case "start", "stop", "restart":
+			if len(cmd.Args) == 0 {
+				fmt.Printf("%s command requires an argument.\n", cmd.Name)
+			} else {
+				fmt.Printf("Executing %s with argument: %s\n", cmd.Name, strings.Join(cmd.Args, " "))
+			}
+		case "reload":
+			fmt.Println("Reloading...")
+		case "exit":
+			fmt.Println("Goodbye!")
+			close(commandChan)
+		default:
+			fmt.Printf("Unknown command: %s\n", cmd.Name)
+		}
+
+		// tell shell reader command has been processed, print new prompt
+		ackChan <- struct{}{}
+	}
+}
 
 // func Init() {
 //     rl, err := readline.New("readline> ")
--- srcs/main.go
+++ srcs/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	// "fmt"
+	"sync"
 	"sync/atomic"
 	"taskmaster/srcs/controller"
-	_ "taskmaster/srcs/input"
+	"taskmaster/srcs/input"
+	"taskmaster/srcs/logging"
 	"taskmaster/srcs/parser"
 	"taskmaster/srcs/signals"
-	"taskmaster/srcs/logging"
 	"time"
 	// _ "github.com/chzyer/readline"
 )
 
 func main() {
+
 	logging.Init("/var/log/taskmaster")
 
 	config := parser.Init("configs/basic.yml")
@@ -20,6 +22,18 @@ func main() {
 	controller.Init(config)
 
 	signals.Init()
+
+	// Channel for shell thread
+	commandChan := make(chan input.Command)
+	ackChan := make(chan struct{})
+	var wg sync.WaitGroup
+
+	// Start shell in a separate thread
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		input.RunShell(commandChan, ackChan)
+	}()
 
 	for signals.FinishProgram == 0 {
 		if atomic.LoadInt32(&signals.ReloadProgram) == 1 {
@@ -29,6 +43,7 @@ func main() {
 			atomic.StoreInt32(&signals.ReloadProgram, 0)
 		}
 		// fmt.Println("Doing things...")
-		time.Sleep(time.Second / 2)
+		input.CheckForCommands(commandChan, ackChan)
+		time.Sleep(time.Second / 4)
 	}
 }
